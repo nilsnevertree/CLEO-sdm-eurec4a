9d6eb30919ffae1dd159ede26105304e54131474
nilsnevertree <49751662+nilsnevertree@users.noreply.github.com>
2024-06-10 11:59:32 +0200

    refactor(eurec4a1d): run eurec4a1d script for all clouds

--- examples/eurec4a1d/eurec4a1d.sh
+++ examples/eurec4a1d/eurec4a1d.sh
@@ -5,7 +5,7 @@
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=128
 #SBATCH --mem=30G
-#SBATCH --time=00:10:00
+#SBATCH --time=01:00:00
 #SBATCH --mail-user=nils-ole.niebaumy@mpimet.mpg.de
 #SBATCH --mail-type=FAIL
 #SBATCH --account=mh1126
@@ -32,20 +32,22 @@ executables="eurec4a1D"
 
 path2CLEO=${HOME}/CLEO/
 path2builds=${path2CLEO}builds/
-path2data=${path2CLEO}data/output/raw/
+path2data=${path2CLEO}data/newoutput/
 path2eurec4a1d=${path2CLEO}examples/eurec4a1d/
 
 # cloud type
 path2sdmeurec4a=${HOME}/repositories/sdm-eurec4a/
-cloud_observation_configfile=${path2sdmeurec4a}data/model/input/new/clusters_18.yaml
+cloud_config_directory=${path2sdmeurec4a}data/model/input/new/
+# cloud_observation_configfile=${path2sdmeurec4a}data/model/input/new/clusters_18.yaml
+# cloud_observation_configfile=${cloud_config_directory}clusters_18.yaml
 
 # Use the stationary or evolving version of the model
 
 ### ---------- Setup for the EUREC4A1D model ---------- ###
 
 # --- stationary version, with super droplet creation at domain top by boundarz conditions
-path2build=${path2builds}build_eurec4a1D_stationary/
-rawdirectory=${path2data}stationary/
+path2build=${path2builds}build_eurec4a1D_stationary_condensation/
+rawdirectory=${path2data}stationary_condensation/
 
 pythonscript=${path2eurec4a1d}scripts/eurec4a1d_stationary.py
 configfile=${path2eurec4a1d}src/config/eurec4a1d_config_stationary.yaml
@@ -57,7 +59,7 @@ configfile=${path2eurec4a1d}src/config/eurec4a1d_config_stationary.yaml
 # rawdirectory=${path2data}evolving/
 
 # create the script arguments
-script_args="${HOME} ${configfile} ${cloud_observation_configfile} ${rawdirectory}"
+# script_args="${HOME} ${configfile} ${cloud_observation_configfile} ${rawdirectory}"
 ### ---------------------------------------------------- ###
 
 
@@ -77,24 +79,36 @@ echo "path2CLEO: ${path2CLEO}"
 echo "path2build: ${path2build}"
 echo "executables: ${executables}"
 echo "pythonscript: ${pythonscript}"
-echo "script_args: ${script_args}"
+# echo "script_args: ${script_args}"
 echo "---------------------------"
 ### ---------------------------------------------------- ###
 
-## ---------------------- build CLEO ------------------ ###
-${path2CLEO}/scripts/bash/build_cleo.sh ${buildtype} ${path2CLEO} ${path2build}
-### ---------------------------------------------------- ###
-
-### --------- compile executable(s) from scratch ---------- ###
-cd ${path2build} && make clean
-
-${path2CLEO}/scripts/bash/compile_cleo.sh ${cleoenv} ${buildtype} ${path2build} "${executables}"
+# ## ---------------------- build CLEO ------------------ ###
+# ${path2CLEO}/scripts/bash/build_cleo.sh ${buildtype} ${path2CLEO} ${path2build}
 # ### ---------------------------------------------------- ###
+
+# ### --------- compile executable(s) from scratch ---------- ###
+# cd ${path2build} && make clean
+
+# ${path2CLEO}/scripts/bash/compile_cleo.sh ${cleoenv} ${buildtype} ${path2build} "${executables}"
+# # ### ---------------------------------------------------- ###
 
 ### --------- run model through Python script ---------- ###
 export OMP_PROC_BIND=spread
 export OMP_PLACES=threads
-${python}  ${pythonscript} ${path2CLEO} ${path2build} ${script_args}
+
+for cloud_configfile in ${cloud_config_directory}/*.yaml; do
+    echo "Running rainshaft1d.py with ${cloud_configfile}"
+    script_args="${HOME} ${configfile} ${cloud_configfile} ${rawdirectory}"
+
+    {
+        ${python}  ${pythonscript} ${path2CLEO} ${path2build} ${script_args}
+
+    } || {
+        echo "Error in running rainshaft1d.py with ${cloud_configfile}"
+    }
+done
+
 ### ---------------------------------------------------- ###
 
 echo "--------------------------------------------"
