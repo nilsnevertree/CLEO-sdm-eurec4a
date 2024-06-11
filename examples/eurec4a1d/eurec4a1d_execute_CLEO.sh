3592818c2ee7194ae1ccbeea90dac78b8b9f8a8e
nilsnevertree <49751662+nilsnevertree@users.noreply.github.com>
2024-06-11 09:22:21 +0200

    refactor(eurec4a1d): seperate the creation of init files and execution of CLEO ( nilsnevertree/CLEO-sdm-eurec4a#35)

---
+++ examples/eurec4a1d/eurec4a1d_execute_CLEO.sh
@@ -0,0 +1,115 @@
+#!/bin/bash
+#SBATCH --job-name=eurec4a1d
+#SBATCH --partition=gpu
+#SBATCH --gpus=4
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=64
+#SBATCH --mem=30G
+#SBATCH --time=01:30:00
+#SBATCH --mail-user=nils-ole.niebaumy@mpimet.mpg.de
+#SBATCH --mail-type=FAIL
+#SBATCH --account=mh1126
+#SBATCH --output=./logfiles/eurec4a1d_run_out.%j.out
+#SBATCH --error=./logfiles/eurec4a1d_run_err.%j.out
+
+### ---------------------------------------------------- ###
+### ------------------ Input Parameters ---------------- ###
+### ------ You MUST edit these lines to set your ------- ###
+### ----- environment, build type, directories, the ---- ###
+### --------- executable(s) to compile and your -------- ###
+### --------------  python script to run. -------------- ###
+### ---------------------------------------------------- ###
+
+echo "--------------------------------------------"
+echo "START RUN"
+date
+echo "git hash: $(git rev-parse HEAD)"
+echo "git branch: $(git symbolic-ref --short HEAD)"
+echo "============================================"
+
+buildtype="cuda"
+executables="eurec4a1D"
+
+path2CLEO=${HOME}/CLEO/
+path2builds=${path2CLEO}builds/
+path2data=${path2CLEO}data/newoutput/
+path2eurec4a1d=${path2CLEO}examples/eurec4a1d/
+
+# cloud type
+path2sdmeurec4a=${HOME}/repositories/sdm-eurec4a/
+cloud_config_directory=${path2sdmeurec4a}data/model/input/new_subset/
+# cloud_observation_configfile=${path2sdmeurec4a}data/model/input/new/clusters_18.yaml
+# cloud_observation_configfile=${cloud_config_directory}clusters_18.yaml
+
+# Use the stationary or evolving version of the model
+
+### ---------- Setup for the EUREC4A1D model ---------- ###
+
+# --- stationary version, with super droplet creation at domain top by boundarz conditions
+path2build=${path2builds}build_eurec4a1D_stationary_condensation/
+
+rawdirectory=${path2data}stationary_condensation/
+
+pythonscript=${path2eurec4a1d}scripts/eurec4a1d_run_executable.py
+
+### ---------------------------------------------------- ###
+
+
+
+### ------------------ Load Modules -------------------- ###
+cleoenv=/work/mh1126/m300950/cleoenv
+python=${cleoenv}/bin/python3
+spack load cmake@3.23.1%gcc
+module load python3/2022.01-gcc-11.2.0
+source activate ${cleoenv}
+### ---------------------------------------------------- ###
+
+### -------------------- print inputs ------------------ ###
+echo "----- Running Example -----"
+echo "buildtype:  ${buildtype}"
+echo "path2CLEO: ${path2CLEO}"
+echo "path2build: ${path2build}"
+echo "executables: ${executables}"
+echo "pythonscript: ${pythonscript}"
+echo "---------------------------"
+### ---------------------------------------------------- ###
+
+# ## ---------------------- build CLEO ------------------ ###
+# ${path2CLEO}/scripts/bash/build_cleo.sh ${buildtype} ${path2CLEO} ${path2build}
+# ### ---------------------------------------------------- ###
+
+# ### --------- compile executable(s) from scratch ---------- ###
+# cd ${path2build} && make clean
+
+# ${path2CLEO}/scripts/bash/compile_cleo.sh ${cleoenv} ${buildtype} ${path2build} "${executables}"
+# # ### ---------------------------------------------------- ###
+
+### --------- run model through Python script ---------- ###
+export OMP_PROC_BIND=spread
+export OMP_PLACES=threads
+
+for cloud_configfile in ${cloud_config_directory}/*.yaml; do
+    echo "::::::::::::::::::::::::::::::::::::::::::::"
+    echo "============================================"
+    echo "RUNNING CLEO EXECUTABLE"
+    echo "with ${cloud_configfile}"
+    echo "============================================"
+
+    script_args="${cloud_configfile} ${rawdirectory}"
+    {
+        ${python}  ${pythonscript} ${path2CLEO} ${path2build} ${script_args}
+    } || {
+        echo "============================================"
+        echo "ERROR: in ${cloud_configfile}"
+        echo "============================================"
+    }
+
+done
+
+
+### ---------------------------------------------------- ###
+
+echo "--------------------------------------------"
+echo "END RUN"
+date
+echo "============================================"
---
+++ examples/eurec4a1d/scripts/eurec4a1d_run_executable.py
@@ -0,0 +1,48 @@
+import os
+import sys
+from pathlib import Path
+import yaml
+import subprocess
+
+print(f"Enviroment: {sys.prefix}")
+
+path2CLEO = Path(sys.argv[1])
+path2build = Path(sys.argv[2])
+cloud_observation_filepath = Path(sys.argv[3])
+rawdirectory = Path(sys.argv[4])
+
+with open(cloud_observation_filepath, "r") as f:
+    cloud_observation_config = yaml.safe_load(f)
+
+
+identification_type = cloud_observation_config["cloud"]["identification_type"]
+cloud_id = cloud_observation_config["cloud"]["cloud_id"]
+
+rawdirectory_individual = rawdirectory / f"{identification_type}_{cloud_id}"
+config_dir = rawdirectory_individual / "config"
+
+config_file = config_dir / "eureca1d_config.yaml"
+dataset_file_str = str(rawdirectory_individual / "eurec4a1d_sol.zarr")
+
+with open(config_file, "r") as f:
+    config_dict = yaml.safe_load(f)
+
+
+print("===============================")
+print("Running CLEO executable")
+os.chdir(path2build)
+os.system("pwd")
+os.system("rm -rf " + dataset_file_str)  # delete any existing dataset
+executable = str(path2build) + "/examples/eurec4a1d/src/eurec4a1D"
+print("Executable: " + executable)
+print("Config file: " + str(config_file))
+os.system(executable + " " + str(config_file))
+
+proc = subprocess.Popen(
+    args=[executable + " " + str(config_file)],
+    stdout=subprocess.PIPE,
+)
+
+(out, err) = proc.communicate()
+
+print("===============================")
