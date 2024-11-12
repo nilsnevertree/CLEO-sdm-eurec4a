67b85116b6ee47a5546eec843a985fb2d9206730
nilsnevertree <nils.niebaum@gmx.de>
2024-11-12 22:18:43 +0100

    feat(pySD): Add LnNormal probdist based on the MSD
    It is related to #56
    The use of a LnNormal distribution in the mass concentration space, allows a better representation of the mass. On the other hand, this lowers the constrain on the number of droplets.

--- pySD/initsuperdropsbinary_src/probdists.py
+++ pySD/initsuperdropsbinary_src/probdists.py
@@ -22,13 +22,53 @@ assuming bins are evenly spaced in log10(r)
 
 import numpy as np
 from scipy import special
+from typing import Tuple, List, Union
 
 
-class CombinedRadiiProbDistribs:
+# Distibution functions
+
+
+def standard_normal(radii: np.ndarray) -> np.ndarray:
+    """Returns probability of each radius in radii according to
+    standard normal distribution
+
+    Parameters:
+    -----------
+    radii : np.ndarray
+        array of radii [m]
+
+    Returns:
+    --------
+    probs : np.ndarray
+        probability of each radius in radii
+    """
+
+    probs = 1 / np.sqrt(2 * np.pi) * np.exp(-(radii**2) / 2)
+    return probs / np.sum(probs)  # normalise so sum(prob) = 1
+
+
+class ProbabilityDistribution:
+    """probability of radius from a probability distribution"""
+
+    def __init__(self):
+        pass
+
+    def __call__(self, radii: np.ndarray) -> np.ndarray:
+        """returns distribution for radii given by the
+        distribution in distrib attribute"""
+
+        raise NotImplementedError("method must be implemented in subclass")
+
+
+class CombinedRadiiProbDistribs(ProbabilityDistribution):
     """probability of radius from the sum of several
     probability distributions"""
 
-    def __init__(self, probdistribs, scalefacs):
+    def __init__(
+        self,
+        probdistribs: Tuple[ProbabilityDistribution],
+        scalefacs: Union[np.ndarray, Tuple[float], List[float]],
+    ):
         self.probdistribs = probdistribs
         self.scalefacs = scalefacs
 
@@ -36,7 +76,7 @@ class CombinedRadiiProbDistribs:
             errmsg = "relative height of each probability distribution must be given"
             raise ValueError(errmsg)
 
-    def __call__(self, radii):
+    def __call__(self, radii: np.ndarray) -> np.ndarray:
         """returns distribution for radii given by the
         sum of the distributions in probdistribs list"""
 
@@ -47,14 +87,14 @@ class CombinedRadiiProbDistribs:
         return probs / np.sum(probs)  # normalise so sum(prob) = 1
 
 
-class DiracDelta:
+class DiracDelta(ProbabilityDistribution):
     """probability of radius nonzero if it is
     closest value in sample of radii to r0"""
 
     def __init__(self, r0):
         self.r0 = r0
 
-    def __call__(self, radii):
+    def __call__(self, radii: np.ndarray) -> np.ndarray:
         """Returns probability of radius in radii sample for
         discrete version of dirac delta function centred on
         value of r in radii closest to r0. For each radius in radii,
@@ -73,7 +113,7 @@ class DiracDelta:
             return np.array([])
 
 
-class VolExponential:
+class VolExponential(ProbabilityDistribution):
     """probability of radius given by exponential in
     volume distribution as defined by Shima et al. (2009)"""
 
@@ -81,7 +121,7 @@ class VolExponential:
         self.radius0 = radius0  # peak of volume exponential distribution [m]
         self.rspan = rspan
 
-    def __call__(self, radii):
+    def __call__(self, radii: np.ndarray) -> np.ndarray:
         """Returns probability of eaach radius in radii according to
         distribution where probability of volume is exponential and bins
         for radii are evently spaced in ln(r).
@@ -100,7 +140,7 @@ class VolExponential:
         return probs / np.sum(probs)  # normalise so sum(prob) = 1
 
 
-class LnNormal:
+class LnNormal(ProbabilityDistribution):
     """probability of radius given by lognormal distribution
     as defined by section 5.2.3 of "An Introduction to clouds from
     the Microscale to Climate" by Lohmann, Luond and Mahrt and radii
@@ -122,7 +162,7 @@ class LnNormal:
             self.geosigs = geosigs
             self.scalefacs = scalefacs
 
-    def __call__(self, radii):
+    def __call__(self, radii: np.ndarray) -> np.ndarray:
         """Returns probability of each radius in radii derived
         from superposition of Logarithmic (in e) Normal Distributions"""
 
@@ -151,7 +191,100 @@ class LnNormal:
         return dn_dlnr
 
 
-class ClouddropsHansenGamma:
+class LnNormalMassSpace(ProbabilityDistribution):
+    """
+    probability of mass concentration by radius following a lognormal distribution.
+    Very similar to the probaility distribution of number concentration given by
+    section 5.2.3 of "An Introduction to clouds from the Microscale to Climate" by Lohmann, Luond and Mahrt.
+    Radii sampled from evenly spaced bins in ln(r).
+
+    Parameters:
+    -----------
+    geomeans : list
+        list of geometric means for each mode [m]
+
+
+    """
+
+    def __init__(
+        self,
+        geomean: float,
+        geosig: float,
+        scalefac: float,
+    ):
+        self.geomean = geomean
+        self.geosig = geosig
+        self.scalefac = scalefac
+
+    def __call__(self, radii: np.ndarray) -> np.ndarray:
+        """Returns probability of each radius in radii derived
+        from superposition of Logarithmic (in e) Normal Distributions"""
+
+        probs = np.zeros(radii.shape)
+        probs = self.__distribution__(radii, self.scalefac, self.geomean, self.geosig)
+
+        return probs / np.sum(probs)  # normalise so sum(prob) = 1
+
+    def __distribution__(
+        self, radii: np.ndarray, geomean: float, geosig: float, scalefac: float
+    ) -> np.ndarray:
+        """calculate probability of radii given the paramters of a
+        lognormal dsitribution according
+        to the mass concentration probability distribution.
+        This is an own implementation which shall help to constrain the total mass concentration.
+        It help to follow the observed mass concentration distribution closer.
+        It is similar to  equation 5.8 of "An Introduction to clouds from the Microscale to Climate"
+        by Lohmann, Luond and Mahrt,
+
+        BUT following the mass concentration distribution.
+        Thus, to gain the number concentration distribution, we need to transform the probability to mass concentration probability.
+        So we need to multiply by 1/(4/3 * pi * rho * r^3).
+        But because we normalise the probability we can just multiply by r^{-3}.
+
+        When creating the multiplicity of the Super-Droplets, one needs to carefully adjust the scale factor:
+        For this if the scalefactor of the MSD is S, the scale factor of the number concentration distribution is:
+        1/(4/3 * pi * rho) * S, with rho being the density of the material.
+
+        Parameters
+        ----------
+        radii : np.ndarray
+            array of radii [m]
+        scalefac : float
+            scale factor for the distribution
+        geomean : float
+            geometric mean of the distribution [m]
+        geosig : float
+            geometric standard deviation of the distribution
+
+        Returns
+        -------
+        result : np.ndarray
+            probability of each radius in radii following the mass concentration probability distribution
+            its units are [m^-3 m^-1]
+        """
+
+        # we can use the expression based on the Standard normal distribution
+        # to calculate the probability of the mass concentration
+        # we need to transform the probability to mass concentration probability
+        # so we need to multiply by 1/(4/3 * pi * r^3)
+        # but because we normalise the probability we can just multiply by r^{-3}
+
+        sigtilda = np.log(geosig)
+        mutilda = np.log(geomean)
+
+        result = (
+            # adjustment to have the number concentration distribution
+            radii ** {-3}
+            # distribution of the mass concentration
+            * scalefac
+            * 1
+            / (sigtilda * radii)
+            * standard_normal(radii=((np.log(radii) - mutilda) / sigtilda))
+        )
+        return result
+
+
+class ClouddropsHansenGamma(ProbabilityDistribution):
     """probability of radius according to gamma distribution for
     shallow cumuli cloud droplets from Poertge et al. 2023"""
 
@@ -159,7 +292,7 @@ class ClouddropsHansenGamma:
         self.reff = reff
         self.nueff = nueff
 
-    def __call__(self, radii):
+    def __call__(self, radii: np.ndarray) -> np.ndarray:
         """return gamma distribution for cloud droplets
         given radius [m] using parameters from Poertge
         et al. 2023 for shallow cumuli (figure 12).
@@ -179,7 +312,7 @@ class ClouddropsHansenGamma:
         return probs / np.sum(probs)  # normalise so sum(prob) = 1
 
 
-class RaindropsGeoffroyGamma:
+class RaindropsGeoffroyGamma(ProbabilityDistribution):
     """probability of radius given gamma distribution for
     shallow cumuli rain droplets from Geoffroy et al. 2014"""
 
@@ -188,7 +321,7 @@ class RaindropsGeoffroyGamma:
         self.qrain = qrain  # rainwater content [g/m^3]
         self.dvol = dvol  # volume mean raindrop diameter [m]
 
-    def __call__(self, radii):
+    def __call__(self, radii: np.ndarray) -> np.ndarray:
         """returns probability of each radius according to a
         gamma distribution for rain droplets using parameters
         from Geoffroy et al. 2014 for precipitating shallow
