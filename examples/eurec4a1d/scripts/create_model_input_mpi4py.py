0fee43700bf8c2fd8d10e935c0a035840f5cefdb
nilsnevertree <49751662+nilsnevertree@users.noreply.github.com>
2025-04-28 11:05:26 +0200

    refactor(eurec4a1d): better use the get_gridboxboundaries to obtain the total number of gridboxes

--- examples/eurec4a1d/scripts/create_model_input_mpi4py.py
+++ examples/eurec4a1d/scripts/create_model_input_mpi4py.py
@@ -422,23 +422,15 @@ for step, cloud_id in enumerate(sublist_cloud_ids):
         constants_filename=constants_file_path,
     )
 
-    with Capturing() as grid_info:
-        rgrid.print_domain_info(constants_file_path, grid_file_path)
-    # extract the total number of gridboxes
-    found_number_gridboxes = False
-    for line in grid_info:
-        if "total no. gridboxes:" in line:
-            grid_dimensions = np.array(
-                line.split(":")[-1].replace(" ", "").split("x"), dtype=int
-            )
-            number_gridboxes_total = int(np.prod(grid_dimensions))
-            found_number_gridboxes = True
-
-    if found_number_gridboxes:
-        number_gridboxes_total = number_gridboxes_total
-    else:
-        number_gridboxes_total = 0
-        raise KeyError("domain no. gridboxes not found in grid_info")
+    try:
+        zhalf, xhalf, yhalf = rgrid.get_gridboxboundaries(
+            grid_filename=grid_file_path,
+            constants_filename=constants_file_path,
+            isprint=False,
+        )
+        number_gridboxes_total = np.size(zhalf)
+    except Exception:
+        raise KeyError("domain no. gridboxes not found")
 
     ### ----- write thermodynamics binaries ----- ###
     logging.info("Create thermodynamics generator")
